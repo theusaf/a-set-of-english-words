c1ab78ece625138cae66fc32feb18f293ff49001
theusaf <theusafyt@gmail.com>
2021-01-13 12:43:55 -0800

    1.0.0 (set-of-english-words) - convert to set, remove cli and stuff

--- test.js
+++ test.js
@@ -1,24 +1,13 @@
-var test = require('tape')
-var words = require('.')
+const words = require('.');
 
-test('words', function(t) {
-  t.ok(Array.isArray(words), 'should be an array')
-  t.ok(words.length > 274000, 'should be more than 274k words')
-  t.ok(
-    words.every(d => typeof d === 'string'),
-    'should be all strings'
-  )
-  t.ok(
-    words.every(d => d === d.toLowerCase()),
-    'should be all lowercase'
-  )
-  t.ok(
-    words.every(d => d.length !== 0),
-    'should not be empty'
-  )
+console.log("Should have over 270,000 words:",words.size > 270e3);
 
-  t.ok(words.includes('fuckton'), 'should include latecomers (#1)')
-  t.ok(words.includes('fucktons'), 'should include latecomers (#2)')
+console.log("Should be a Set:",words instanceof Set);
 
-  t.end()
-})
+const fun = new Set();
+words.forEach((w)=>{
+  if(/fun/.test(w)){
+    fun.add(w);
+  }
+});
+console.log("Should have the characters 'fun' in them:",fun);
